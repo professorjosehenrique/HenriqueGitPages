f69dc6eb6bf189d10122730d5f409763a1519d52
professorjosehenrique <169004521+professorjosehenrique@users.noreply.github.com>
2024-05-19 10:36:22 -0300

    Create Henrique.sketch.js
    Adicionando programa js

---
+++ Henrique.sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background( " white ");
+  
+}
+function draw() {
+  
+  stroke("blue");
+  
+  fill( "red");
+  
+  // console.log(mouseIsPressed) {
+  
+  if (mouseIsPressed)
+    
+    rect(mouseX, mouseY, 20 ,35);
+}
